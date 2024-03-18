9fa26e0239005f4f825cd6acebbe51b8db341700
tobiaskane69 <tobiashernandez47@gmail.com>
2024-03-18 22:30:37 +0800

    .

--- app/src/main/java/com/example/myportfolioapp/MainActivity.java
+++ app/src/main/java/com/example/myportfolioapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myportfolioapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,11 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     EditText input_name;
-    EditText input_email;
-    EditText input_address;
-    EditText input_number;
-    EditText input_about;
+    EditText input_age;
     EditText input_skills;
-
+    EditText input_address;
+    EditText input_phone;
+    TextView user_output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +27,14 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        input_name = (EditText) findViewById(R.id.name_text);
-        input_email = (EditText) findViewById(R.id.email_text);
-        input_address = (EditText) findViewById(R.id.address_text);
-        input_number = (EditText) findViewById(R.id.number_text);
-        input_about= (EditText) findViewById(R.id.about_text);
-        input_skills = (EditText) findViewById(R.id.skills_text);
+        input_name = (EditText) findViewById(R.id.nameInput);
+        input_age = (EditText) findViewById(R.id.ageInput);
+        input_skills = (EditText) findViewById(R.id.skillInput);
+        input_address = (EditText) findViewById(R.id.emailInput);
+        input_phone = (EditText) findViewById(R.id.phoneInput);
+        user_output = (TextView) findViewById(R.id.userOutput);
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -39,12 +42,13 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
-    public void Save(View v){
-        input_name = findViewById(R.id.name_text);
-        input_email = findViewById(R.id.email_text);
-        input_address = findViewById(R.id.address_text);
-        input_number = findViewById(R.id.number_text);
-        input_about= findViewById(R.id.about_text);
-        input_skills = findViewById(R.id.skills_text);
+
+    @SuppressLint("SetTextI18n")
+    public void handleText(View v){
+        input_name = findViewById(R.id.nameInput);
+        input_age = findViewById(R.id.ageInput);
+        input_address = findViewById(R.id.emailInput);
+        input_phone = findViewById(R.id.phoneInput);
+        user_output.setText("NAME: " + input_name.getText() + "\nAGE: " + input_age.getText() + " years old. \nADDRESS: " + input_skills.getText() + "\nEMAIL: " + input_address.getText() + "\nPHONE #: " + input_phone.getText());
     }
 }
